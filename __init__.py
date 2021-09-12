d178b594c5642fd658727d3a084abaa73776bdc7
Jeong-Jingyo <sqidward1208@naver.com>
2021-09-12 17:21:27 +0900

    fix

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import sys
+import asyncio
 import serial
 import configparser
 import pathlib
@@ -7,47 +9,59 @@ cfg.read(str(pathlib.Path(__file__).parent.resolve()) + "/serial.cfg")
 port = cfg["config"]["port"]
 baud_rate = cfg["config"]["baud_rate"]
 
-try:
-    ser = serial.Serial(port=port)
-except serial.SerialException as ex:
-    print(ex)
 
+class Serial:
+    working = None
+    latest_bin_allocation_ratio = None
+    ser = None
 
-def door(open_door: bool):
-    pass
-    # print(open_door)
-    # if open_door:
-    #     ser.write(b"1")
-    # else:
-    #     ser.write(b"2")
+    def __init__(self):
+        try:
+            self.ser = serial.Serial(port=port)
+        except serial.SerialException as ex:
+            print(ex)
 
+    async def get_stat(self):
+        while True:
+            stat = self.ser.read()
+            try:
+                int(stat)
+                self.latest_bin_allocation_ratio = int(stat)
+            except ValueError:
+                if stat == "i":
+                    self.working = False
+                elif stat == "w":
+                    self.working = True
 
-def door_closed() -> bool:
-    return False
+    def door(self):
+        pass
 
+    def door_closed(self) -> bool:
+        return False
 
-def home(): ser.write(b"2b")
+    def home(self):
+        self.ser.write(b"2b")
 
+    def bin1(self):
+        self.ser.write(b"1a")
 
-def bin1(): ser.write(b"1a")
+    def bin2(self):
+        self.ser.write(b"1c")
 
+    def bin3(self):
+        self.ser.write(b"3a")
 
-def bin2(): ser.write(b"1c")
+    def bin4(self):
+        self.ser.write(b"3b")
 
+    def bin5(self):
+        self.ser.write(b"4a")
 
-def bin3(): ser.write(b"3a")
+    def bin6(self):
+        self.ser.write(b"4c")
 
+    def bin7(self):
+        self.ser.write(b"5a")
 
-def bin4(): ser.write(b"3b")
-
-
-def bin5(): ser.write(b"4a")
-
-
-def bin6(): ser.write(b"4c")
-
-
-def bin7(): ser.write(b"5a")
-
-
-def bin8(): ser.write(b"5c")
+    def bin8(self):
+        self.ser.write(b"5c")
